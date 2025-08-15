5075b412438045aa0b5c19bb1502811d7c1e8ad1
JairoG26 <ja.garavito@uniandes.edu.co>
2025-08-15 16:31:38 -0500

    Server fix 2

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,13 @@ const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
 
+// Configurar CORS
+app.use(cors({
+  origin: 'https://task-manager-j26.netlify.app', // tu frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true // si usas cookies o sesiones
+}));
+
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
