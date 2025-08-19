ac0b1c994998daf1b00c24dc9dcfd8ff29060cba
JairoG26 <ja.garavito@uniandes.edu.co>
2025-08-19 10:29:04 -0500

    Swagger ruote

--- backend/src/swagger.js
+++ backend/src/swagger.js
@@ -10,7 +10,7 @@ const options = {
       description: 'API para gestionar tareas',
     },
     servers: [
-      { url: '/api' } // 👈 así todas las rutas empiezan con /api
+      { url: 'https://task-manager-backend-1l6d.onrender.com/api' }
     ],
     components: {
       schemas: {
